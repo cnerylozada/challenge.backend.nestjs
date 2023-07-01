c885ac6c751c22ccd5049ef63e09d6e7b99016c6
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-07-01 15:06:15 -0500

    chore: show env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './config/typeorm.config';
 import { QuizzesModule } from './quizzes/quizzes.module';
 
-console.log(typeormConfig);
+console.log(
+  process.env.POSTGRESQL_HOST,
+  process.env.POSTGRESQL_PORT,
+  process.env.POSTGRESQL_USER,
+  process.env.POSTGRESQL_PASSWORD,
+  process.env.POSTGRESQL_DB,
+);
 @Module({
   imports: [TypeOrmModule.forRoot(typeormConfig), QuizzesModule],
   controllers: [],
