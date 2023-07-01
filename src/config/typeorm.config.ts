b83ecbe23007703a8d72cf79f835a9b0e0a7e6ec
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-07-01 11:35:35 -0500

    feat: set databases env keys

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeormConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: '35.223.120.48',
-  port: 5432,
-  username: 'postgres',
-  password: '123456',
-  database: 'postgres',
+  host: process.env.POSTGRESQL_HOST,
+  port: Number(process.env.POSTGRESQL_PORT) || 5432,
+  username: process.env.POSTGRESQL_USER || 'postgres',
+  password: process.env.POSTGRESQL_PASSWORD || '123456',
+  database: process.env.POSTGRESQL_DB || 'potsgres',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
 };
