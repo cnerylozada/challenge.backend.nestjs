29828772e90c714bf98f245900bc7679e6f2e24c
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-07-01 15:02:29 -0500

    chore: show typeorm-config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './config/typeorm.config';
 import { QuizzesModule } from './quizzes/quizzes.module';
 
+console.log(typeormConfig);
 @Module({
   imports: [TypeOrmModule.forRoot(typeormConfig), QuizzesModule],
   controllers: [],
