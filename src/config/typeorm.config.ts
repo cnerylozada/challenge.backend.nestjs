46bff8bf196872114fab8820deb1ddda11cab47e
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-07-02 12:07:55 -0500

    chore: remove console logs

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeormConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.POSTGRESQL_HOST || '35.223.120.48',
+  host: process.env.POSTGRESQL_HOST || '34.136.252.69',
   port: Number(process.env.POSTGRESQL_PORT) || 5432,
   username: process.env.POSTGRESQL_USER || 'postgres',
   password: process.env.POSTGRESQL_PASSWORD || '123456',
