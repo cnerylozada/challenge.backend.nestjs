4a73a1a31a82d29f90fa58e675da3e9dc5ea8a79
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-07-03 14:50:47 -0500

    feat: validation by time

--- src/quizzes/services/addresses.service.ts
+++ src/quizzes/services/addresses.service.ts
@@ -18,8 +18,16 @@ export class AddressesService {
       const timeNow = Date.now();
       const lastTime = new Date(address.lastTime).getTime();
       const elapsedMinutes = (timeNow - lastTime) / (1000 * 60);
-      console.log('elapsedMinutes', elapsedMinutes);
-      return elapsedMinutes > 40;
+      let result = false;
+      if (elapsedMinutes > 5) {
+        await this.addressesRepository.save({
+          id: address.id,
+          address: address.address,
+          lastTime: new Date().toISOString(),
+        });
+        result = true;
+      } else result = false;
+      return result;
     } catch (error) {
       return false;
     }
